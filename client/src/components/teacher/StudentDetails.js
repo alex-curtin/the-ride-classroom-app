360001855c64212600d5fe3f922cd685ffe6cbd6
Alex <alexhcurtin@gmail.com>
2020-02-11 10:20:05 -0500

    Add student details component and test

---
+++ client/src/components/teacher/StudentDetails.js
@@ -0,0 +1,54 @@
+import React from 'react';
+
+const StudentDetails = ({
+  student: {
+    id,
+    bio: {
+      givenName,
+      familyName,
+      nickName,
+      email,
+      age,
+      grade
+    },
+    history: {
+      absences,
+      gpa
+    },
+    grades
+  }
+}) => {
+  return (
+    <div data-test='component-student-details'>
+      <h2>{givenName} {familyName}</h2>
+      <p>Student id: <span>{id}</span></p>
+      <p>Grade: <span>{grade}</span></p>
+      <p>Age: <span>{age}</span></p>
+      <p>Email: <span>{email}</span></p>
+      <p>Nickname: <span>'{nickName}'</span></p>
+      <p>GPA: <span>{gpa}</span></p>
+      <p>Number of absences: <span>{absences}</span></p>
+      <div>
+        <h3>Grades</h3>
+        <table>
+          <thead>
+            <tr>
+              <th>Project</th>
+              <th>Grade</th>
+            </tr>
+          </thead>
+          <tbody>
+            {Object.keys(grades).map(grade => (
+              <tr key={grade}>
+                <td>{grade}</td>
+                <td>{grades[grade]}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  )
+}
+
+export default StudentDetails;
---
+++ client/src/components/teacher/StudentDetails.test.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import { shallow, mount } from 'enzyme';
+
+import StudentDetails from './StudentDetails';
+import { findByTestAttr } from '../../testUtils.js';
+
+const defaultProps = {
+  student: {
+    id: "3b0d7462-8ba1-4a5c-8a64-7721f7a9947c",
+    bio: {
+      givenName: "Aisling",
+      familyName: "Worthington",
+      nickName: "Aisey",
+      email: "aisey@email.me",
+      age: 15,
+      grade: 10
+    },
+    history: {
+      absences: 5,
+      gpa: 3.7
+    },
+    grades: {
+      project1: 92,
+      project2: 88,
+      project3: 91,
+      project4: "n/a"
+    }
+  }
+}
+
+/**
+ * Creates a ShallowWrapper for StudentDetails component
+ * @param {object} props - props for this setup.
+ * @returns {ShallowWrapper}
+ */
+const setup = (props = {}) => {
+  const setupProps = { ...defaultProps, ...props };
+  return shallow(<StudentDetails {...setupProps} />);
+};
+
+test('renders without error', () => {
+  const wrapper = setup();
+  const component = findByTestAttr(wrapper, 'component-student-details');
+  expect(component.length).toBe(1);
+})
