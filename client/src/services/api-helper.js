b74601e0af574649fc9dfe3d65e8a7fbfed87711
Alex <alexhcurtin@gmail.com>
2020-02-10 23:37:16 -0500

    Add api call helper functions

---
+++ client/src/services/api-helper.js
@@ -0,0 +1,42 @@
+import axios from 'axios';
+
+const BASE_URL = 'http://localhost:5000/api';
+
+/**
+ * Get all students data from API.
+ * @function getStudents
+ * @returns {array} - Array of students.
+ */
+export const getStudents = async () => {
+  const resp = await axios.get(`${BASE_URL}/students`);
+  return resp.data;
+};
+
+/**
+ * Get all dates data from API.
+ * @function getDates
+ * @returns {array} - Array of dates.
+ */
+export const getDates = async () => {
+  const resp = await axios.get(`${BASE_URL}/dates`);
+  return resp.data;
+};
+
+/**
+ * Get all desks data from API.
+ * @function getDesks
+ * @returns {array} - Array of desks.
+ */
+export const getDesks = async () => {
+  const resp = await axios.get(`${BASE_URL}/desks`);
+  return resp.data;
+};
+
+/**
+ * Get all rooms data from API.
+ * @function getRooms
+ * @returns {array} - Array of rooms.
+ */
+export const getRooms = asynce() => {
+  const resp = await axios.get(`${BASE_URL}/rooms`);
+};
