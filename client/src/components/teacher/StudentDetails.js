01d71933176a85e83822036b0f20eb58b72d55ca
Alex <alexhcurtin@gmail.com>
2020-02-12 12:01:51 -0500

    Add loadStudents to student details component and add tests

--- client/src/components/teacher/StudentDetails.js
+++ client/src/components/teacher/StudentDetails.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 /**
  * Functional React component for a single student details.
@@ -6,53 +6,65 @@ import React from 'react';
  * @param {object} props - React props.
  * @returns {JSX.Element} - Rendered component.
  */
-const StudentDetails = ({
-  student: {
-    id,
-    bio: {
-      givenName,
-      familyName,
-      nickName,
-      email,
-      age,
-      grade
-    },
-    history: {
-      absences,
-      gpa
-    },
-    grades
-  }
-}) => {
-  return (
-    <div data-test='component-student-details' className='component-student-details'>
-      <h2 className='page-headline'>{givenName} {familyName}</h2>
-      <p>Student id: <span>{id}</span></p>
-      <p>Grade: <span>{grade}</span></p>
-      <p>Age: <span>{age}</span></p>
-      <p>Email: <span>{email}</span></p>
-      <p>Nickname: <span>'{nickName}'</span></p>
-      <p>GPA: <span>{gpa}</span></p>
-      <p>Number of absences: <span>{absences}</span></p>
-      <div>
-        <h3>Grades</h3>
-        <table>
-          <thead>
-            <tr>
-              <th>Project</th>
-              <th>Grade</th>
-            </tr>
-          </thead>
-          <tbody>
-            {Object.keys(grades).map(grade => (
-              <tr key={grade}>
-                <td>{grade}</td>
-                <td>{grades[grade]}</td>
+const StudentDetails = ({ student, loadStudents }) => {
+  useEffect(() => {
+    if (!student) {
+      loadStudents();
+    }
+  }, [loadStudents, student]);
+
+  if (student) {
+    const {
+      id,
+      bio: {
+        givenName,
+        familyName,
+        nickName,
+        email,
+        age,
+        grade
+      },
+      history: {
+        absences,
+        gpa
+      },
+      grades
+    } = student;
+
+    return (
+      <div data-test='component-student-details' className='component-student-details'>
+        <h2 className='page-headline'>{givenName} {familyName}</h2>
+        <p>Student id: <span>{id}</span></p>
+        <p>Grade: <span>{grade}</span></p>
+        <p>Age: <span>{age}</span></p>
+        <p>Email: <span>{email}</span></p>
+        <p>Nickname: <span>'{nickName}'</span></p>
+        <p>GPA: <span>{gpa}</span></p>
+        <p>Number of absences: <span>{absences}</span></p>
+        <div>
+          <h3>Grades</h3>
+          <table>
+            <thead>
+              <tr>
+                <th>Project</th>
+                <th>Grade</th>
               </tr>
-            ))}
-          </tbody>
-        </table>
+            </thead>
+            <tbody>
+              {Object.keys(grades).map(grade => (
+                <tr key={grade} data-test='grade-item'>
+                  <td>{grade}</td>
+                  <td>{grades[grade]}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
       </div>
+    )
+  } else return (
+    <div>
+      loading...
     </div>
   )
 }
--- client/src/components/teacher/StudentDetails.test.js
+++ client/src/components/teacher/StudentDetails.test.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import { shallow, mount } from 'enzyme';
+import { BrowserRouter as Router } from 'react-router-dom';
 
 import StudentDetails from './StudentDetails';
 import { findByTestAttr } from '../../testUtils.js';
@@ -38,8 +39,52 @@ const setup = (props = {}) => {
   return shallow(<StudentDetails {...setupProps} />);
 };
 
-test('renders without error', () => {
-  const wrapper = setup();
-  const component = findByTestAttr(wrapper, 'component-student-details');
-  expect(component.length).toBe(1);
-})
+/**
+ * Mounts component, for testing lifecycle methods.
+ * @param {object} props - props for this setup.
+ * @returns {ReactWrapper}
+ */
+const setupMount = (props = {}) => {
+  const setupProps = { ...defaultProps, ...props };
+  return mount(
+    <Router>
+      <StudentDetails {...setupProps} />
+    </Router>);
+};
+
+describe('StudentDetails component', () => {
+  test('should render without error', () => {
+    const wrapper = setup();
+    const component = findByTestAttr(wrapper, 'component-student-details');
+    expect(component.length).toBe(1);
+  });
+
+  test('should render all grades', () => {
+    const wrapper = setup();
+    const gradeItems = findByTestAttr(wrapper, 'grade-item');
+    const expectedResult = Object.keys(defaultProps.student.grades).length
+    expect(gradeItems.length).toBe(expectedResult);
+  })
+
+  describe('loadStudents', () => {
+    test('should run if `students` prop is null', () => {
+      const loadStudentsMock = jest.fn();
+      const props = { student: null, loadStudents: loadStudentsMock };
+
+      const wrapper = setupMount(props);
+
+      const loadStudentsCallCount = loadStudentsMock.mock.calls.length;
+      expect(loadStudentsCallCount).toBe(1);
+    });
+
+    test('should not run if `students` prop is not null', () => {
+      const loadStudentsMock = jest.fn();
+      const props = { loadStudents: loadStudentsMock };
+
+      const wrapper = setupMount(props);
+
+      const loadStudentsCallCount = loadStudentsMock.mock.calls.length;
+      expect(loadStudentsCallCount).toBe(0);
+    });
+  });
+});
